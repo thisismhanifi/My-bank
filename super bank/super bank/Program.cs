b493e469656fc8b6310a2b716e02595a2d6a5da1
Mohammad Hanifi <82605371+thisismhanifi@users.noreply.github.com>
2021-08-22 05:27:46 +0430

    Add files via upload

---
+++ super bank/super bank/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace super_bank
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            var account = new bankAccount("ali", +10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}. ");
+
+            account.Makewithdrawal(120, DateTime.Now, "Hammock");
+            
+            account.Makewithdrawal(50, DateTime.Now, "Xbox Game");
+
+            Console.WriteLine(account.GetAccountHistory());
+            Console.WriteLine($"your    Balance is   :   {account.Balance}");
+
+            //Test for a negative balance:
+            try
+            {
+                account.Makewithdrawal(75000, DateTime.Now, "attempt to overdraw");
+
+            }
+            catch (InvalidOperationException e)
+            {
+
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new bankAccount("invalid", -55);
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+                Console.WriteLine("Exception caught crerating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+
+        }
+    }
+}
---
+++ super bank/super bank/bankAccount.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace super_bank
+{
+    public class bankAccount
+    {
+        
+        public string Number { get; }
+        public string Owner { get; set; }
+
+        public decimal Balance
+        { 
+            get
+            {
+               decimal balance = 0;
+                foreach (var item in alltransaction)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+            set { }
+            
+
+
+            
+
+        }
+
+        private static int accountNumberseed = 1234567890;
+
+        private List<Transaction> alltransaction = new List<Transaction>();
+
+        public bankAccount(string name,decimal initialBalance)
+        {
+            // in this type [ this ] is not necessary .
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "intial Balance");
+            this.Number = accountNumberseed.ToString();
+            accountNumberseed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            alltransaction.Add(deposit);
+        }
+
+        public void Makewithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance + amount < 0)
+            {
+                throw new InvalidOperationException("not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            alltransaction.Add(withdrawal);
+
+
+
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            //HEADER
+            report.AppendLine("Date\tAmount\tNote");
+            foreach (var item in alltransaction)
+            {
+                //ROWS
+                report.AppendLine($"{item.Date.Year}\t{item.Amount}\t{item.Notes}");
+            }
+
+            return report.ToString();
+            
+
+        }
+    }
+}
